f691c9ed17a7e208cd725deac31a886f154e71e4
Jennifer Dobak <jdobak1@gmail.com>
2018-01-15 20:56:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
   else array.length >= 3
-    array.join(" , ")
+    "#{array[0..-2].join(", ")} and #{array.last}"
 end
 end
